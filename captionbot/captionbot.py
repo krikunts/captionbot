577c50bfa4f23460021cfd13299c7482b31dbbe6
lucky-user <krikunts@gmail.com>
2016-05-09 12:08:59 +0300

    raising CaptionBotException when getting HTTP error

--- captionbot/captionbot.py
+++ captionbot/captionbot.py
@@ -6,15 +6,29 @@ import logging
 logger = logging.getLogger("captionbot")
 
 
+class CaptionBotException(Exception):
+    pass
+
+
 class CaptionBot:
     BASE_URL = "https://www.captionbot.ai/api/"
+
+    @staticmethod
+    def _resp_error(resp):
+        if not resp.ok:
+            data = resp.json()
+            msg = "HTTP error: {}".format(resp.status_code)
+            if type(data) == dict and "Message" in data:
+                msg += ", " + data.get("Message")
+            raise CaptionBotException(msg)
 
     def __init__(self):
         self.session = requests.Session()
         url = self.BASE_URL + "init"
         resp = self.session.get(url, verify=False)
         logger.debug("init: {}".format(resp))
-        self.conversation_id = json.loads(resp.text)
+        self._resp_error(resp)
+        self.conversation_id = resp.json()
         self.watermark = ''
 
     def _upload(self, filename):
@@ -24,7 +38,8 @@ class CaptionBot:
         files = {'file': (name, open(filename, 'rb'), mime)}
         resp = self.session.post(url, files=files, verify=False)
         logger.debug("upload: {}".format(resp))
-        return json.loads(resp.text)
+        self._resp_error(resp)
+        return resp.json()
 
     def url_caption(self, image_url):
         data = json.dumps({
@@ -38,9 +53,8 @@ class CaptionBot:
         url = self.BASE_URL + "message"
         resp = self.session.post(url, data=data, headers=headers, verify=False)
         logger.debug("url_caption: {}".format(resp))
-        if not resp.ok:
-            return None
-        res = json.loads(json.loads(resp.text))
+        self._resp_error(resp)
+        res = json.loads(resp.json())
         self.watermark = res.get("WaterMark")
         return res.get("UserMessage")
 
